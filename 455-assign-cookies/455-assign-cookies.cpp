8c9cf080a2469bb3f53e13ac8b5ff0609361764d
Priyam Gupta <priyamgupta880@gmail.com>
2022-05-22 23:10:22 +0530

    Time: 41 ms (41.31%), Space: 17.5 MB (79.25%) - LeetHub

---
+++ 455-assign-cookies/455-assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+       sort(g.begin(),g.end());
+       sort(s.begin(),s.end());
+        int p1 = 0;
+        int p2 = 0;
+        
+        while(p1<g.size() && p2 < s.size()){
+            if(g[p1] <= s[p2]){
+                p1++;
+            p2++; 
+        } else{
+                p2++;
+            }
+    }
+         return p1;
+    }
+};
