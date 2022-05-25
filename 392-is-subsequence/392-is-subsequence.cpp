ca4314ce17c6448fc55e3a5e7e5074d573a6bb3c
Priyam Gupta <priyamgupta880@gmail.com>
2022-05-25 10:38:32 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (31.91%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+         int i = 0;
+        int j = 0;
+        
+        int n = s.size();
+        int m = t.size();
+        
+        while(i < n && j < m) {
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        
+        return i == n;
+    }
+};
