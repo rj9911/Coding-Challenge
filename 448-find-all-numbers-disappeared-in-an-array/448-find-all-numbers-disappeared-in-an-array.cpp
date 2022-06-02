76b6cdb26a92903e335ef2e2b8e7560b99dc88a7
Priyam Gupta <priyamgupta880@gmail.com>
2022-06-02 10:26:40 +0530

    Time: 113 ms (33.09%), Space: 33.8 MB (59.66%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -14,7 +14,7 @@ public:
         }
         
       return v; */
-      map<int , int>mp;
+    /*  map<int , int>mp;
         int n = nums.size();
       vector<int> v;
       for(int i=0;i<n;i++){
@@ -25,6 +25,20 @@ public:
             v.push_back(i);
         }
     }
+        return v;
+        */
+       vector<int>v;
+       int n = nums.size();
+       for(int i = 0 ; i < n ;i++){
+           int mark = abs(nums[i]) - 1;
+           if(nums[mark] > 0){
+               nums[mark] *= -1;
+           }
+       } 
+        for(int i = 0 ; i< n; i++){
+            if(nums[i] > 0)
+                v.push_back(i+1);
+        }
         return v;
     }
 };
