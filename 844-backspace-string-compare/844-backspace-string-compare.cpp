5c1d502eb4f58624a014bfd9d2034c16b4eb612b
Priyam Gupta <priyamgupta880@gmail.com>
2022-06-03 14:37:28 +0530

    Time: 6 ms (15.32%), Space: 6.1 MB (97.24%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+       int x =0 ; 
+        int y = 0;
+        for(int i = 0 ; i< s.size() ;i++){
+            if(s[i] == '#'){    
+                x--;
+                x =max(0,x);
+            }else{
+                s[x] = s[i];
+                x++;
+            }
+        }
+          for(int i = 0 ; i< t.size() ;i++){
+            if(t[i] == '#'){    
+                y--;
+                y =max(0,y);
+            }else{
+                t[y] = t[i];
+                y++;
+            }
+        }
+        if(x != y) return false;
+        else{
+            for(int i= 0 ; i< x;i++){
+                if(s[i] != t[i]) return false;
+            }
+            return true;
+        }
+    }
+};
