5468c2fce8f096338ad4c9b0107afc33f01728c6
Priyam Gupta <priyamgupta880@gmail.com>
2022-06-05 17:27:38 +0530

    Time: 3 ms (89.03%), Space: 8.8 MB (89.62%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -7,26 +7,10 @@ public:
   
         while(start < end){
             int mid = start + (end - start)/2;
-          if(mid > 0 && mid < n-1){
-            if(arr[mid] > arr[mid+1] && arr[mid]>arr[mid-1])
-                return mid;
-            else if(arr[mid-1] < arr[mid])
+            if(arr[mid] > arr[mid+1])
+                end = mid;
+            else
                 start = mid+1;
-            else
-                end = mid- 1;
-        }
-        else if(mid== 0){
-            if(arr[0] > arr[1])
-                return 0;
-            else
-                return 1;
-        }
-        else if(mid == n - 1){
-            if(arr[n-1] > arr[n-2])
-                return n-1;
-            else 
-                return n-2;
-        }
         }
         return start;
     }
