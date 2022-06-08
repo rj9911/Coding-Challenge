d3275dcbb935c582e08a13a22a6415fb878a28d8
Priyam Gupta <priyamgupta880@gmail.com>
2022-06-08 07:44:43 +0530

    Time: 21 ms (36.99%), Space: 7.1 MB (91.12%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // Same as Analgram Question
+    bool checkInclusion(string s1, string s2) {
+      unordered_map<char ,int>mp;
+       for(auto nums:s1){
+           mp[nums]++;
+       }
+       int j = 0 , k =s1.size() , count=mp.size() , n=s2.size();
+       int i = 0;
+        while(j<n){
+            if(mp.find(s2[j]) != mp.end()){
+            mp[s2[j]]--;
+            if(mp[s2[j]] == 0)
+                count--;
+            }
+            if(j-i+1 < k)
+                j++;
+            else if(j-i+1 == k){
+                if(count == 0){
+                    return true;
+                }
+                if(mp.find(s2[i]) != mp.end()){
+                mp[s2[i]]++;
+               if(mp[s2[i]] == 1)
+                   count++;
+                }
+                i++;
+                j++;
+            }
+        }
+        return false;    
+    }
+};
