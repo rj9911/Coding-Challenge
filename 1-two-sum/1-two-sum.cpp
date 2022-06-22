ecb7d3863965f8c7309d679971f42763a12085e2
Priyam Gupta <priyamgupta880@gmail.com>
2022-06-22 20:28:51 +0530

    Time: 16 ms (71.65%), Space: 10.3 MB (61.51%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-      /*  vector<int >ans;
-        unordered_map<int , int>mpp;
-        for(int i = 0 ; i < nums.size(); i++ ){
-            if(mpp.find(target - nums[i]) != mpp.end()){
-                ans.push_back(mpp[target - nums[i]]);
-                ans.push_back(i);
-                return ans;
-            }   
-            else{
-                mpp[nums[i]] = i;
-            }
-        }
-        return ans;*/
-        vector<int>res , temp;
+        vector<int> v,temp;
         temp = nums;
         
         sort(nums.begin() , nums.end());
         int n = nums.size();
-        int left = 0, right = n - 1;
-     while(left<right){
-         if(nums[left ] + nums[right] == target){
-             break;
-         }
-         else if(nums[left ] + nums[right] < target)  left++;
-         else right--;
-     }
-        for(int i = 0 ; i< temp.size() ; i++){
-            if(temp[i] == nums[left])
-                res.push_back(i);
-           else if(temp[i] == nums[right])
-                res.push_back(i);
+        
+        int left = 0, right = n-1;
+        while(left< right){
+            if(nums[left] + nums[right] == target)
+                break;
+            else if(nums[left] + nums[right] > target)
+                right--;
+            else
+                left++;
         }
-        return res;
+        for(int i =0 ; i<temp.size() ;i++){
+           if(temp[i] == nums[left])
+               v.push_back(i);
+           else if(temp[i] == nums[right])
+               v.push_back(i);
+        }
+        return v;
     }
 };
