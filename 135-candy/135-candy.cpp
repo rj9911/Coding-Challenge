d97347ad40c5a2b5715ec3355349cb1fb42cb883
Priyam Gupta <priyamgupta880@gmail.com>
2022-07-05 18:08:03 +0530

    Time: 22 ms (90.56%), Space: 18.1 MB (36.23%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -24,18 +24,19 @@ public:
                int size = ratings.size();
          if(size<2)
              return size;
-         vector<int> l2r(size,1),r2l(size,1);
+         vector<int> lv(size,1);
+      vector<int>rv(size,1);
          for(int i=1;i<size;i++){
              if(ratings[i-1]<ratings[i])
-                 l2r[i] = l2r[i-1]+1;
+                 lv[i] = lv[i-1]+1;
          }
          for(int i=size-2;i>=0;i--){
              if(ratings[i+1]<ratings[i])
-                 r2l[i] = r2l[i+1]+1;
+                 rv[i] = rv[i+1]+1;
          }
          int sum = 0;
          for(int i=0;i<size;i++){
-             sum+=max(l2r[i],r2l[i]);
+             sum+=max(lv[i],rv[i]);
          }
          return sum;
 
