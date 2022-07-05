2672930b6edfde0ae6c80e86f0fab49dd9cd96f8
Priyam Gupta <priyamgupta880@gmail.com>
2022-07-05 18:21:22 +0530

    Time: 41 ms (31.74%), Space: 18.3 MB (13.41%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
   int candy(vector<int>& ratings) {
-    /*    int n = a.size();
-        int ans = 0;
-       vector<int> rv(n,1);
-       vector<int> lv(n,1);
+        int size = ratings.size(); 
+       vector<int> lv(size,1);
+       vector<int> rv(size,1);
       
-      for(int i= 1; i< n ; i++){
-          if(a[i] > a[i-1]){
-              rv[i] += 1;
+      for(int i= 1; i< size; i++){
+          if(ratings[i-1] < ratings[i]){
+              lv[i] = lv[i-1]+ 1;
           }
       }
-       for(int i= n-2; i >= 0 ; i--){
-          if(a[i] > a[i+1]){
-              lv[i] += 1;
+       for(int i= size-2; i >= 0 ; i--){
+          if(ratings[i+1] < ratings[i]){
+              rv[i] = rv[i+1] + 1;
           }
       }
-      for(int i =0 ; i< n;i++){
+      int ans=0;
+      for(int i =0 ; i< size;i++){
          ans += max(lv[i] ,rv[i]); 
       }
       return ans;
-      */
+     /*
                int size = ratings.size();
          vector<int> lv(size,1);
       vector<int>rv(size,1);
@@ -37,6 +37,6 @@ public:
              sum+=max(lv[i],rv[i]);
          }
          return sum;
-
+*/
     }
 };
