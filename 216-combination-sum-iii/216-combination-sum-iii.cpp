a024e587d43d93378e5af9c5ac6945254f3487d3
Priyam Gupta <priyamgupta880@gmail.com>
2022-07-24 15:32:21 +0530

    Time: 4 ms (37.54%), Space: 6.7 MB (41.62%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void help(int i ,vector<int>& C, int t, vector<int>& sol, int k){
+    void help(int i , int t, vector<int>& sol, int k){
         if(t == 0 && k == 0){
             ans.push_back(sol);
             return;
         }
         if(t < 0 || k< 0) return; //  As Not a good case.
-        if(i == C.size()) return; // As we will not get anything after that.
+        if(i == 10) return; // As we will not get anything after that.
         
         // Skip the ith elem
-        help(i+1, C, t,sol,k);
+        help(i+1, t,sol,k);
         
         // pick the ith elem
-        sol.push_back(C[i]);
-        help(i+1, C, t-C[i] , sol,k-1);  //
+        sol.push_back(i);
+        help(i+1, t-i , sol,k-1);  //Don't include the same i again.
         
         // Backtracking
         sol.pop_back();
     
     }
     vector<vector<int>> combinationSum3(int k, int t) {
-        vector<int> candidates = {1,2,3,4,5,6,7,8,9}; 
-                vector<int> sol;
-        help(0, candidates, t, sol, k);
+     //   vector<int> candidates = {1,2,3,4,5,6,7,8,9};
+    
+        vector<int> sol;
+        help(1, t, sol, k);
         return ans;
     }
 };
