1e9bd78c2102e6cee370791ef652985cfc1339f8
Priyam Gupta <priyamgupta880@gmail.com>
2022-07-24 12:27:36 +0530

    Time: 8 ms (25.23%), Space: 15.9 MB (23.35%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public://Time = Space = O(2^n) as we are making Descision tree.
     void solve(int i, vector<int>& nums,vector<int> temp,vector<vector<int>>& result){
         if(nums.size() == i){
             result.push_back(temp);
@@ -8,14 +8,18 @@ public:
             // to take the ith element.
             temp.push_back(nums[i]);
             solve(i+1, nums,temp, result);
+            // Backtracking is same as recursion
             
-            temp.pop_back();
+            temp.pop_back(); // This step is Backtracking
+            // Backtracking is to undue some changes that we did in line      9.,and see result
             // To ignore the ith element.
+            
             solve(i+1, nums,temp, result);
         }
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
+        // This is working only for n < 20 after than it won't works.
         vector<int> temp;
         vector<vector<int>> result;
         solve(0, nums,temp, result);
