832cb6d4361b1630dc37b85014d9228770a9457a
Priyam Gupta <priyamgupta880@gmail.com>
2022-07-28 20:06:21 +0530

    Time: 31 ms (51.00%), Space: 23.3 MB (48.63%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,44 +1,51 @@
 class Solution {
 public:
     
-    vector<int> dr = {0, -1, 0, 1};
-    vector<int> dc = {1, 0, -1, 0};
-    int M = 0, N = 0;
+    bool isvalid(vector<vector<int>>& grid, int i, int j, int n, int m){
+        if(i>=0 && i<n && j>=0 && j<m && grid[i][j] == 1){
+            return true;
+        }
+        return false;
+    }
+    
+    void dfs(vector<vector<int>>& grid, int i, int j, int n, int m, int &area){
+        area++;
+        grid[i][j] = 0;
+        
+        if(isvalid(grid, i+1, j ,n , m)){
+            dfs(grid, i+1, j, n, m, area);
+        }
+        
+        if(isvalid(grid, i-1, j ,n , m)){
+            dfs(grid, i-1, j, n, m, area);
+        }
+        
+        if(isvalid(grid, i, j+1 ,n , m)){
+            dfs(grid, i, j+1, n, m, area);
+        }
+        
+        if(isvalid(grid, i, j-1 ,n , m)){
+            dfs(grid, i, j-1, n, m, area);
+        }
+        
+    }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         
-        this->M = grid.size();
-        this->N = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
         
-        vector<vector<bool>> isViz(M, vector<bool>(N, false));
+        int ans = 0;
         
-        int maxArea = 0;
-        
-        for(int i=0;i<M;i++){
-            for(int j=0;j<N;j++){
-                if(grid[i][j] && !isViz[i][j]){
-                    maxArea = max(maxArea, islandArea(i, j, grid, isViz));
-                }        
+        for(int i = 0; i< n; i++){
+            for(int j= 0; j<m; j++){
+                if(grid[i][j] == 1){
+                    int area = 0;
+                    dfs(grid, i, j, n, m, area);
+                    ans= max(ans, area);
+                }
             }
         }
-        
-        return maxArea;
-    }
-    
-    bool isWithin(int r, int c){
-        return (r >= 0 && r < M && c >= 0 && c < N);
-    }
-    
-    int islandArea(int r, int c, vector<vector<int>>& grid, vector<vector<bool>>& isViz){
-        
-        isViz[r][c] = true;
-        int area = 1;
-        
-        for(int i = 0;i<4;i++){
-            if(isWithin(r + dr[i], c + dc[i]) && grid[r + dr[i]][c + dc[i]] && !isViz[r + dr[i]][c + dc[i]]){
-                area += islandArea(r + dr[i], c + dc[i], grid, isViz);
-            }
-        }
-        return area;
+        return ans;
     }
 };
