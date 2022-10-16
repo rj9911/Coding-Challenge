5129d40d79861d98f972b6caa251762e2ffdb0f9
Priyam Gupta <priyamgupta880@gmail.com>
2022-10-16 11:54:10 +0530

    Time: 14 ms (97.05%), Space: 9.8 MB (16.52%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        vector<int>v;
+        ListNode *temp =head;
+        while(temp){
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        sort(v.begin(),v.end());
+        temp=head;
+        int i= 0;
+        while(temp && i<v.size()){
+            temp->val=v[i++];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
