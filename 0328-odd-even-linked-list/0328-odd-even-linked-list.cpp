39fcf08dc978465ca17740348981feb1ebb001bb
Priyam Gupta <priyamgupta880@gmail.com>
2022-10-17 11:09:43 +0530

    Time: 33 ms (12.01%), Space: 10.9 MB (5.30%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -18,19 +18,20 @@ public:
             temp=temp->next;
         }
         for(int i=0;i<v.size();i++){
-            if(i%2==0) odd.push_back(v[i]);
-            else if(i%2) even.push_back(v[i]);
+            if(i%2) odd.push_back(v[i]);
+            else if(i%2==0) even.push_back(v[i]);
         }
         
         temp=head;
-        for(int i=0;i<odd.size();i++){
-            temp->val=odd[i];
-            temp=temp->next;
-        }
         for(int i=0;i<even.size();i++){
             temp->val=even[i];
             temp=temp->next;
         }
+        for(int i=0;i<odd.size();i++){
+            temp->val=odd[i];
+            temp=temp->next;
+        }
+
         return head;
     }
 };
