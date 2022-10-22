b83157f34095ad18538bddb5126bbdb84c1e8876
Priyam Gupta <priyamgupta880@gmail.com>
2022-10-22 15:02:05 +0530

    Time: 11 ms (96.62%), Space: 15.7 MB (89.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int>mp;
-        vector<int> v;
-        int n= nums.size();
-        for(auto i : nums){
-            mp[i]++;
+       // Extension of Moore's Voting Algo ( Two elements can be possible appears
+       //greater than n/3)
+       vector<int> v;
+       int count1=0 , count2=0 , ele1= INT_MIN, ele2= INT_MIN; // ele1 can also be
+       // -ve or zero.
+       int n = nums.size();
+       for(int i=0; i<n;i++){
+           if(ele1 == nums[i]) count1++;
+           else if(ele2 == nums[i]) count2++;
+           else if(count1== 0){
+               ele1 = nums[i];
+               count1++;
+           }else if(count2 == 0){
+               ele2 = nums[i];
+               count2++;
+           }else{
+               count1--; count2--;
+           }
+       }
+       count1 =0; count2=0;
+       for(int i=0;i<n; i++){
+           if(ele1==nums[i]) count1++;
+           if(ele2==nums[i]) count2++;
         }
-        for(auto i :mp){
-            if(i.second > abs(n/3)) v.push_back(i.first);
-        }
-        return v;
+       if(count1 > n/3) v.push_back(ele1);
+       if(count2 > n/3) v.push_back(ele2);
+       return v;
     }
 };
