303e226a3428d7a0147b01ba3c83692eb90c4e1b
Priyam Gupta <priyamgupta880@gmail.com>
2022-10-30 10:17:03 +0530

    Time: 28 ms (5.51%), Space: 14.8 MB (42.46%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -42,13 +42,13 @@ public:
           }
         }
 }
-            if(head1 != NULL){
+            while(head1 != NULL){
                 ft -> next = head1;
-               // head1=head1->next;
+                head1=head1->next;  ft=ft->next;
             }
-            if(head2 != NULL){
+            while(head2 != NULL){
                 ft ->next = head2;
-                //head2=head2->next;
+                head2=head2->next;  ft=ft->next;
             }
         return fh;
     }
