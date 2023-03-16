cc51300410d12379c5725f1c53351e1d7acbf57a
Priyam Gupta <priyamgupta880@gmail.com>
2023-03-16 06:29:33 +0530

    Added solution - LeetHub

--- Bit Difference - GFG/bit-difference.cpp
+++ Bit Difference - GFG/bit-difference.cpp
@@ -11,14 +11,14 @@ using namespace std;
 class Solution{
     public:
     // Function to find number of bits needed to be flipped to convert A to B
-    int countBitsFlip(int a, int b){
-       int x = a^b,cnt=0;
-       while(x>0){
-           if(x&1 == 1)
-              cnt++;
-           x >>= 1;
+    int countBitsFlip(int a, int b){    // a=01010 , b=10100
+       int x = a^b,cnt=0;    // x=01010 ^ 10100 -> 11110
+       while(x>0){          // ans = total set bits in the number
+           if(x&1 == 1)     // 11110 -> 1111 -> 111 -> 11 -> 1 -> 0
+              cnt++;        //   0 ->     1 ->   2 ->   3 -> 4 
+           x >>= 1;         
        }
-       return cnt;
+       return cnt;        // 4
     }
 };
 
