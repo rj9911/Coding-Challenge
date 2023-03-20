30325149acf6a4666903472c497ebcdf3b6a5014
Priyam Gupta <priyamgupta880@gmail.com>
2023-03-20 17:06:43 +0530

    Time: 5 ms (31.27%), Space: 6.6 MB (71.32%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
    int combinationSum4(vector<int>& nums, int target) {
-    vector<unsigned int> dp(target + 1, 0);
-    dp[0] = 1;
+    vector<unsigned int> dp(target + 1, 0); //represents the number of possible combinations that add up to the target i
+    dp[0] = 1; // there is exactly one way to get a target of 0 (by not choosing any number).
     for (int i = 1; i <= target; i++) {
         for (int num : nums) {
             if (num <= i) {
-                dp[i] += dp[i - num];
+                dp[i] += dp[i - num]; //  number of ways to get a target of i - num
             }
         }
     }
     return dp[target];
 }
+// This problem has the property of "optimal substructure", which means that the solution to the problem can be obtained by combining the solutions to its subproblems.
 
 };
