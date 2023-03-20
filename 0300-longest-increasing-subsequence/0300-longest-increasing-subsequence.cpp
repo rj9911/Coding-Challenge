f87df0944ddb523b938b24543139033e052c97bc
Priyam Gupta <priyamgupta880@gmail.com>
2023-03-20 14:34:24 +0530

    Time: 287 ms (66.33%), Space: 10.6 MB (57.66%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,19 +3,19 @@ public:
     
     int lengthOfLIS(vector<int>& nums) {
         // Time = n2 space = O(n)
-        
+        // Pepcoding
         int n = nums.size();
         vector<int> dp(n+1,0);
         int omax=0;
         for(int i=0;i<n;i++){
             int maxi=0;
-            for(int j=0;j<i;j++){
+            for(int j=0;j<i;j++){ // We are finding the max value before the ith value
                 if(nums[j]<nums[i])
                     maxi = max(maxi, dp[j]);
             }
-            dp[i] = maxi+1;
-            
-            if(dp[i] > omax) omax = dp[i];
+            dp[i] = maxi+1;  // increasing by one on that val
+             
+            if(dp[i] > omax) omax = dp[i];      
         }
         
         return omax;
